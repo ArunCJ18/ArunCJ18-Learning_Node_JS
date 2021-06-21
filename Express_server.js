2506a895878f17cb82a1ce2bbfde75aaf813f7cf
ArunCJ18 <arunjeyamurugan88@gmail.com>
2021-06-21 19:45:58 +0530

    After installing nodemon for autorestart

--- Express_server.js
+++ Express_server.js
@@ -1,8 +1,8 @@
-const express = require ("express");
+const express = require("express");
 
 const app = express();
 
-app.use((req, res, next)=>{
+/* app.use((req, res, next)=>{
     console.log("I am a middleware!")
     next();                      //without next() the function won't jump or execute next function  
 });
@@ -10,4 +10,25 @@ app.use((req, res, next)=>{
 app.use((req, res, next)=>{
     console.log("I am second user middleware")
 });
-app.listen(3000)
+app.listen(3000) */
+
+app.use("/", (req, res, next) => {
+    console.log("This always Print"); //bcoz its use one thread it executes line by line
+    console.log("This is firmware 0.0");
+    next();  //we're using next bcoz its common firmware for both '/' and '/page2'
+});
+
+app.use("/Page2", (req, res, next) => {  //we're declaring /page2 first bcoz "/"== "/page2" && "/"
+    console.log("Its Firmware 0.2");
+    res.send("<h1>You're at Page 2</h1>");
+    //if we put next() here js try to execute the next firmware, that is 2 pages at a time
+    //this will lead to "Cannot set headers after they are sent to the client"
+});
+
+app.use("/", (req, res, next) => {
+    console.log("Its Firmware 0.1");
+    res.send("<h1>You're at page 1</h1>"); //Express js automatically add HTML properties to this
+});   //
+
+
+app.listen(3000);
