866d7489b205bac3698971370474a0ab8f1f1501
Arunkumaran J <arunjeyamurugan88@gmail.com>
2021-06-26 21:50:04 +0530

    added some ext css to nodejs and static file

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = express();
 const shopRouter = require("./routes/shop.js")
 const adminRouter = require("./routes/admin.js")
 
+//always checks for file in this folder if a file that we asked is in this folder
+//exists it loads the file => this now used for the css in html
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin",adminRouter);
 app.use(shopRouter); //include '/admin' for that file before the path 
 
